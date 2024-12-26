137b14332b915c42bfb88a203c8fad81ae470b85
wenjin <hewenjin94@outlook.com>
2024-12-26 22:21:44 +0800

    feat(neovim): use the neogen branch that supports mini.snippets

--- lua/plugin/editing.lua
+++ lua/plugin/editing.lua
@@ -320,7 +320,10 @@ later(function()
   map("n", "<leader>cS", "<cmd>ScissorsEditSnippet<cr>", "Snippet edit")
   map({ "n", "x" }, "<leader>cs", "<cmd>ScissorsAddNewSnippet<cr>", "Snippet add")
 
-  add("danymat/neogen")
-  require("neogen").setup()
+  add({
+    source = "echasnovski/neogen",
+    checkout = "mini-snippets",
+  })
+  require("neogen").setup({ snippet_engine = "mini" })
   map("n", "<leader>cA", "<cmd>Neogen<cr>", "Generate annotation")
 end)
