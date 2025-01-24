f76f7a5c953ec0e379281a7db3502ed543b7cfe5
wenjin <hewenjin94@outlook.com>
2025-01-24 22:28:58 +0800

    feat(theme): adjust wallpaper and set theme to gruvbox

--- colors/wenvim-brown.lua
+++ colors/wenvim-brown.lua
@@ -1,8 +1,8 @@
 ---@diagnostic disable: param-type-mismatch
 local base_color = {
-  background = "#1d2021",
-  foreground = "#fbf1c7",
-  saturation = "high",
+  background = "#202020",
+  foreground = "#ddc7a1",
+  saturation = "mediumhigh",
 }
 require("mini.hues").setup(base_color)
 require("util.color").setup_wenvim_color(base_color)
--- lua/plugin/colorscheme.lua
+++ lua/plugin/colorscheme.lua
@@ -3,5 +3,5 @@ if vim.g.vscode then return end
 local now = MiniDeps.now
 now(function()
   require("mini.colors").setup()
-  vim.cmd.colorscheme("wenvim-dark")
+  vim.cmd.colorscheme("wenvim-brown")
 end)
