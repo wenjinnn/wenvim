076dcb12f60595c8749cdaddcfb52bc265ce0f25
wenjin <hewenjin94@outlook.com>
2025-02-07 20:52:27 +0800

    Revert "feat(neovim): don't save blank and terminal buffer for mksession"
    This reverts commit 7602db9903b0e2b0dd6c2831b4f1045c2d58f587.

--- lua/util/init.lua
+++ lua/util/init.lua
@@ -9,6 +9,23 @@ function M.setup_term_opt(event)
   vim.b.miniindentscope_disable = true
 end
 
+-- Delete all dap terminals, useful when session restored, cuz in that timing sometimes we have some dead dap terminals.
+function M.delete_dap_terminals()
+  local dap_terminals_output = vim.api.nvim_exec2("filter /\\[dap-terminal\\]/ buffers", { output = true })
+  local dap_terminals = vim.split(dap_terminals_output.output, "\n")
+  local buffers_index = {}
+  for _, terminal in ipairs(dap_terminals) do
+    local buf_args = vim.split(vim.trim(terminal), " ")
+    local buf_index = tonumber(buf_args[1])
+    if buf_index ~= nil then
+      table.insert(buffers_index, buf_index)
+    end
+  end
+  if #buffers_index > 0 then
+    vim.cmd("bd! " .. vim.fn.join(buffers_index, " "))
+  end
+end
+
 -- Wrapper for QoL
 function M.keycode(key)
   return vim.api.nvim_replace_termcodes(key, true, true, true)
