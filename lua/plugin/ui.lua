07512720b20a857e34ad85a47ad7e96bdaa3e726
wenjin <hewenjin94@outlook.com>
2025-02-26 10:23:09 +0800

    fix(neovim): mini.clue lead mini.files mark not work

--- lua/plugin/ui.lua
+++ lua/plugin/ui.lua
@@ -91,9 +91,7 @@ later(function()
       { mode = "n", keys = "g" },
       { mode = "x", keys = "g" },
       -- Marks
-      { mode = "n", keys = "'" },
       { mode = "n", keys = "`" },
-      { mode = "x", keys = "'" },
       { mode = "x", keys = "`" },
       -- Registers
       { mode = "n", keys = '"' },
