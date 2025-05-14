0b2de687788bf04d2d673537ff397765645c8e1f
wenjin <hewenjin94@outlook.com>
2025-05-14 10:19:00 +0800

    chore(neovim): bring back orignal `;` `,` function for f forward

--- lua/plugin/motion.lua
+++ lua/plugin/motion.lua
@@ -1,9 +1,12 @@
 local later = MiniDeps.later
 
 later(function()
+  local map = require('util').map
   require('mini.jump').setup()
   require('mini.jump2d').setup({ view = { dim = true } })
+  map('n', ';', function() MiniJump.jump(nil, false) end, 'Jump forward')
+  map('n', ',', function() MiniJump.jump(nil, true) end, 'Jump backward')
   -- Personally, I prefer using mini.jump2d's query. With it, the number of keystrokes is almost constant
   local function jump2d_query() MiniJump2d.start(MiniJump2d.builtin_opts.query) end
-  require('util').map({ 'n', 'x', 'o' }, '<CR>', jump2d_query, 'Jump2d query')
+  map({ 'n', 'x', 'o' }, '<CR>', jump2d_query, 'Jump2d query')
 end)
