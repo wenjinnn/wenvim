6726fbc265c0ea84688ecaa20e8dfd895cc054af
wenjin <hewenjin94@outlook.com>
2025-05-22 20:17:43 +0800

    Revert "Reapply "feat(neovim): add mcphub.nvim""
    This reverts commit fef59c43005ebd969f7d1b537bb36b503800a798.

--- lua/plugin/extra.lua
+++ lua/plugin/extra.lua
@@ -173,14 +173,9 @@ later(function()
   add({
     source = 'olimorris/codecompanion.nvim',
     depends = {
-      'ravitemer/mcphub.nvim',
       'ravitemer/codecompanion-history.nvim',
       'Davidyz/VectorCode',
     },
-  })
-  require('mcphub').setup({
-    cmd = 'npx',
-    cmdArgs = { '--yes', '--', 'mcp-hub@latest' },
   })
   local default_adapter = os.getenv('NVIM_AI_ADAPTER') or 'copilot'
   local ollama_model = os.getenv('NVIM_OLLAMA_MODEL') or 'deepseek-r1:14b'
@@ -228,14 +223,6 @@ later(function()
     extensions = {
       history = {
         enabled = true,
-      },
-      mcphub = {
-        callback = 'mcphub.extensions.codecompanion',
-        opts = {
-          show_result_in_chat = true, -- Show mcp tool results in chat
-          make_vars = true, -- Convert resources to #variables
-          make_slash_commands = true, -- Add prompts as /slash commands
-        },
       },
       vectorcode = {
         opts = {
