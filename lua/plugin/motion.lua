df35c253e3f9cecec84a50ee4b53b683ef3f0d95
wenjin <hewenjin94@outlook.com>
2025-05-24 17:02:28 +0800

    chore(neovim): adjust ; , mapping mode

--- lua/plugin/motion.lua
+++ lua/plugin/motion.lua
@@ -4,8 +4,8 @@ later(function()
   local map = require('util').map
   require('mini.jump').setup()
   require('mini.jump2d').setup({ view = { dim = true } })
-  map('n', ';', function() MiniJump.jump(nil, false) end, 'Jump forward')
-  map('n', ',', function() MiniJump.jump(nil, true) end, 'Jump backward')
+  map({ 'n', 'x', 'o' }, ';', function() MiniJump.jump(nil, false) end, 'Jump forward')
+  map({ 'n', 'x', 'o' }, ',', function() MiniJump.jump(nil, true) end, 'Jump backward')
   -- Personally, I prefer using mini.jump2d's query. With it, the number of keystrokes is almost constant
   local function jump2d_query() MiniJump2d.start(MiniJump2d.builtin_opts.query) end
   map({ 'n', 'x', 'o' }, '<CR>', jump2d_query, 'Jump2d query')
