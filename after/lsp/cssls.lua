effdef82de00e00fe461bd48417e5c8ceca192c3
wenjin <hewenjin94@outlook.com>
2025-05-28 19:46:06 +0800

    chore(neovim): uncommented cssls config

--- after/lsp/cssls.lua
+++ after/lsp/cssls.lua
@@ -1,7 +1,7 @@
--- local capabilities = require('util.lsp').make_capabilities()
--- capabilities.textDocument.completion.completionItem.snippetSupport = true
--- return {
---   {
---     capabilities = capabilities,
---   },
--- }
+local capabilities = require('util.lsp').make_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+return {
+  {
+    capabilities = capabilities,
+  },
+}
