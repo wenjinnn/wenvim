23a2aedc221a4f292ebf54e9c86b4c4eb7751b52
wenjin <hewenjin94@outlook.com>
2025-07-03 16:14:43 +0800

    fix(neovim): treesitter-context didn't enable by default

--- lua/plugin/editing.lua
+++ lua/plugin/editing.lua
@@ -147,6 +147,7 @@ later(function()
     end,
   })
   require('nvim-ts-autotag').setup()
+  require('treesitter-context').setup()
   local function go_to_context() require('treesitter-context').go_to_context(vim.v.count1) end
   map('n', '[e', go_to_context, 'treesitter context upward')
 end)
