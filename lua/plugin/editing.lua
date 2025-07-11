e893bbf3052b5ba2c72087ebfba86efae93ddab0
wenjin <hewenjin94@outlook.com>
2025-07-11 11:49:12 +0800

    feat(neovim): add flake8 lint for python

--- lua/plugin/editing.lua
+++ lua/plugin/editing.lua
@@ -240,7 +240,9 @@ later(function()
   local lint = require('lint')
   -- just use the default lint
   -- TODO maybe add more linter in future
-  lint.linters_by_ft = {}
+  lint.linters_by_ft = {
+    python = {'flake8'}
+  }
   vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
     callback = function()
       lint.try_lint()
