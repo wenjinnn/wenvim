7e29b431df67f0c3bbd69de77118766c9e25924c
wenjin <hewenjin94@outlook.com>
2025-07-22 17:51:06 +0800

    Revert "chore(neovim): adjust completeopt"
    This reverts commit afc0d9db537627772ea2c1ab0c3888034debf434.

--- plugin/options.lua
+++ plugin/options.lua
@@ -69,7 +69,7 @@ opt.confirm = true
 opt.updatetime = 500
 opt.fileencodings:append({ 'gbk', 'cp936', 'gb2312', 'gb18030', 'big5', 'euc-jp', 'euc-kr', 'prc' })
 opt.termguicolors = true
-opt.completeopt = { 'menuone', 'noinsert', 'fuzzy' }
+opt.completeopt = { 'menuone', 'noselect', 'fuzzy' }
 opt.pumheight = 20
 opt.sessionoptions:remove({ 'blank' })
 opt.wildmode = 'longest:full,full'
