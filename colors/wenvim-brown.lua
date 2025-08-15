a41728fb231d1d106128ff28be5834fa714911bc
wenjin <hewenjin94@outlook.com>
2025-08-15 10:23:34 +0800

    feat(neovim): adjust wenvim-brown color

--- colors/wenvim-brown.lua
+++ colors/wenvim-brown.lua
@@ -1,8 +1,7 @@
 ---@diagnostic disable: param-type-mismatch
 local base_color = {
-  background = '#202020',
-  foreground = '#ddc7a1',
-  saturation = 'mediumhigh',
+  background = '#1d2021',
+  foreground = '#bdae93',
 }
 require('mini.hues').setup(base_color)
 require('util.color').setup_wenvim_color(base_color)
