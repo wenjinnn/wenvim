74d4af88cf58e95a713fa1360ec623219e46baea
wenjin <hewenjin94@outlook.com>
2025-08-15 12:39:08 +0800

    chore(neovim): adjust wenvim-brown color

--- colors/wenvim-brown.lua
+++ colors/wenvim-brown.lua
@@ -1,6 +1,6 @@
 ---@diagnostic disable: param-type-mismatch
 local base_color = {
-  background = '#1d2021',
+  background = '#202020',
   foreground = '#bdae93',
 }
 require('mini.hues').setup(base_color)
