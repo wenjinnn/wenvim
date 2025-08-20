9f552b48a8768377d1762be37e20f558baa6da41
wenjin <hewenjin94@outlook.com>
2025-08-20 23:03:50 +0800

    chore(neovim): update runner and other keymaps

--- lua/wenvim/plugin/runner.lua
+++ lua/wenvim/plugin/runner.lua
@@ -1,8 +1,14 @@
 local add, later = MiniDeps.add, MiniDeps.later
 
 later(function()
+  local map = require('wenvim.util').map
   add('kassio/neoterm')
   add('tpope/vim-dispatch')
   add('vim-test/vim-test')
   vim.g['test#strategy'] = 'neoterm'
+  map('n', '<leader>tn', '<cmd>TestNearest<CR>', 'Run nearest test')
+  map('n', '<leader>tf', '<cmd>TestFile<CR>', 'Run file tests')
+  map('n', '<leader>ts', '<cmd>TestSuite<CR>', 'Run test suite')
+  map('n', '<leader>tl', '<cmd>TestLast<CR>', 'Run last test')
+  map('n', '<leader>tv', '<cmd>TestVisit<CR>', 'Visit test file')
 end)
