a533f0e160b578d8a4ad05ba7e63377b47b253a3
wenjin <hewenjin94@outlook.com>
2025-08-27 21:23:30 +0800

    chore(neovim): simplify mini.basics setup

--- lua/wenvim/plugin/basic.lua
+++ lua/wenvim/plugin/basic.lua
@@ -15,11 +15,4 @@ end)
 
 later(function() require('mini.extra').setup() end)
 
-later(function()
-  require('mini.basics').setup({
-    extra_ui = true,
-    mappings = { windows = false },
-  })
-  -- disable mini.basics C-s mapping
-  vim.keymap.del({ 'n', 'i', 'x' }, '<C-s>')
-end)
+later(function() require('mini.basics').setup() end)
