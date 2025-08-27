a843aef5f1e5639c5e30871851809510b9dc545a
wenjin <hewenjin94@outlook.com>
2025-08-27 17:03:21 +0800

    chore(neovim): adjust vim-fugitive Gclog format

--- lua/wenvim/plugin/git.lua
+++ lua/wenvim/plugin/git.lua
@@ -4,6 +4,7 @@ local map = require('wenvim.util').map
 -- git
 later(function()
   add('tpope/vim-fugitive')
+  vim.g.fugitive_summary_format = '%ch | %an | %s'
   require('mini.diff').setup()
   map('n', '<leader>go', MiniDiff.toggle_overlay, 'Git toggle overlay')
 end)
