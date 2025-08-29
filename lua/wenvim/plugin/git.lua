76327ee0cf25619af5bdaaaa31d650e198a22249
wenjin <hewenjin94@outlook.com>
2025-08-29 11:32:18 +0800

    feat(neovim): readd mini.git setup

--- lua/wenvim/plugin/git.lua
+++ lua/wenvim/plugin/git.lua
@@ -5,6 +5,10 @@ local map = require('wenvim.util').map
 later(function()
   add('tpope/vim-fugitive')
   vim.g.fugitive_summary_format = '%ch | %an | %s'
+  require('mini.git').setup()
   require('mini.diff').setup()
+  map({ 'n', 'x' }, '<leader>ga', MiniGit.show_at_cursor, 'Git show at cursor')
+  map({ 'n', 'v' }, '<leader>gh', MiniGit.show_range_history, 'Git show range history')
+  map({ 'n', 'v' }, '<leader>gd', MiniGit.show_diff_source, 'Git show diff source')
   map('n', '<leader>go', MiniDiff.toggle_overlay, 'Git toggle overlay')
 end)
