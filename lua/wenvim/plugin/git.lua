6bae17744e36a196783c42b452ed6859e031d93b
wenjin <hewenjin94@outlook.com>
2025-08-29 11:40:59 +0800

    feat(neovim): let vim-fugitive handle git command

--- lua/wenvim/plugin/git.lua
+++ lua/wenvim/plugin/git.lua
@@ -3,10 +3,11 @@ local map = require('wenvim.util').map
 
 -- git
 later(function()
-  add('tpope/vim-fugitive')
-  vim.g.fugitive_summary_format = '%ch | %an | %s'
   require('mini.git').setup()
   require('mini.diff').setup()
+  -- setup vim-fugitive after mini.git so that `:G` `:Git` can override mini.git's
+  add('tpope/vim-fugitive')
+  vim.g.fugitive_summary_format = '%ch | %an | %s'
   map({ 'n', 'x' }, '<leader>ga', MiniGit.show_at_cursor, 'Git show at cursor')
   map({ 'n', 'v' }, '<leader>gh', MiniGit.show_range_history, 'Git show range history')
   map({ 'n', 'v' }, '<leader>gd', MiniGit.show_diff_source, 'Git show diff source')
