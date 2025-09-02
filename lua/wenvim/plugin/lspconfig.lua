19fa4b9843e8cb81dea3d4e8760c64252d4b779d
wenjin <hewenjin94@outlook.com>
2025-09-02 13:57:57 +0800

    chore(neovim): add lsp document color toggle keymap

--- lua/wenvim/plugin/lspconfig.lua
+++ lua/wenvim/plugin/lspconfig.lua
@@ -45,9 +45,8 @@ later(function()
     callback = util_lsp.on_detach,
   })
   -- finally, some LSP related keymaps
-  local function inlay_hint_toggle()
-    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = 0 }), { bufnr = 0 })
-  end
+  local function inlay_hint_toggle() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end
+  local function document_color_toggle() vim.lsp.document_color.enable(not vim.lsp.document_color.is_enabled()) end
   local function list_workspace_folders() vim.notify(vim.inspect(vim.lsp.buf.list_workspace_folders())) end
   map({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, 'Lsp code action')
   map('n', '<leader>k', vim.lsp.buf.signature_help, 'Lsp signature help')
@@ -56,7 +55,8 @@ later(function()
   map('n', '<leader>cR', vim.lsp.buf.rename, 'Lsp rename')
   map('n', '<leader>cl', vim.diagnostic.setloclist, 'Lsp diagnostic location list')
   map('n', '<leader>cI', vim.lsp.buf.incoming_calls, 'Lsp incoming calls')
-  map('n', '<leader>ch', vim.lsp.buf.outgoing_calls, 'Lsp outgoing calls')
+  map('n', '<leader>cO', vim.lsp.buf.outgoing_calls, 'Lsp outgoing calls')
   map('n', '<leader>cH', inlay_hint_toggle, 'Lsp inlay hint toggle')
+  map('n', '<leader>ch', document_color_toggle, 'Lsp document color toggle')
   map('n', '<leader>cL', list_workspace_folders, 'Lsp list workspace folder')
 end)
