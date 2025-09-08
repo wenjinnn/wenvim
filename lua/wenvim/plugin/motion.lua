37c47cc472bc58d79f340d7e5eef53addfd06405
wenjin <hewenjin94@outlook.com>
2025-09-08 17:56:43 +0800

    chore(neovim): adjust `;` `,` to MiniJump.smart_jump

--- lua/wenvim/plugin/motion.lua
+++ lua/wenvim/plugin/motion.lua
@@ -4,16 +4,11 @@ later(function()
   local map = require('wenvim.util').map
   require('mini.jump').setup()
   require('mini.jump2d').setup({
-    view = {
-      dim = true,
-      n_steps_ahead = 2,
-    },
-    mappings = {
-      start_jumping = 'sj',
-    },
+    view = { dim = true, n_steps_ahead = 2, },
+    mappings = { start_jumping = 'sj', },
   })
-  map({ 'n', 'x', 'o' }, ';', function() MiniJump.jump(nil, false) end, 'Jump forward')
-  map({ 'n', 'x', 'o' }, ',', function() MiniJump.jump(nil, true) end, 'Jump backward')
+  map({ 'n', 'x', 'o' }, ';', function() MiniJump.smart_jump(false) end, 'Jump forward')
+  map({ 'n', 'x', 'o' }, ',', function() MiniJump.smart_jump(true) end, 'Jump backward')
   local function jump2d_single_chatacter() MiniJump2d.start(MiniJump2d.builtin_opts.single_character) end
   map({ 'n', 'x', 'o' }, 'ss', jump2d_single_chatacter, 'Jump2d single chatacter')
   local function jump2d_query() MiniJump2d.start(MiniJump2d.builtin_opts.query) end
