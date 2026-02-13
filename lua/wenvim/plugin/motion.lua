748c17a71deb368439ebff484389fb421558f125
wenjin <hewenjin94@outlook.com>
2026-02-13 10:10:10 +0800

    feat(neovim): revert mini.jump2d default mapping

--- lua/wenvim/plugin/motion.lua
+++ lua/wenvim/plugin/motion.lua
@@ -1,9 +1,6 @@
 local map = require('wenvim.util').map
 require('mini.jump').setup()
-require('mini.jump2d').setup({
-  view = { dim = true, n_steps_ahead = 2 },
-  mappings = { start_jumping = 'sj' },
-})
+require('mini.jump2d').setup({ view = { dim = true, n_steps_ahead = 2 } })
 map({ 'n', 'x', 'o' }, ';', function() MiniJump.smart_jump(false) end, 'Jump forward')
 map({ 'n', 'x', 'o' }, ',', function() MiniJump.smart_jump(true) end, 'Jump backward')
 local function jump2d_single_chatacter() MiniJump2d.start(MiniJump2d.builtin_opts.single_character) end
