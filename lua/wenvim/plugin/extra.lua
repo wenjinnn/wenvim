c0bda001debb3be7165177bf70a491b5031559b9
wenjin <hewenjin94@outlook.com>
2026-04-01 22:41:17 +0800

    chore(neovim): readd codecompanion customization settings

--- lua/wenvim/plugin/extra.lua
+++ lua/wenvim/plugin/extra.lua
@@ -41,7 +41,43 @@ later(function()
 
   -- AI assistant
   vim.pack.add({ gh('olimorris/codecompanion.nvim'), gh('ravitemer/codecompanion-history.nvim') })
-  require('codecompanion').setup({ extensions = { history = { enabled = true } } })
+  local default_adapter = os.getenv('NVIM_AI_ADAPTER') or 'copilot'
+  local ollama_model = os.getenv('NVIM_OLLAMA_MODEL') or 'deepseek-r1:14b'
+  local openrouter_model = os.getenv('NVIM_OPENROUTER_MODEL') or 'anthropic/claude-sonnet-4.6'
+  local api_key_cmd = "cmd:sops exec-env $SOPS_SECRETS 'echo -n $%s'"
+  local ollama_setting = { schema = { model = { default = ollama_model } } }
+
+  local function extend_adapter(adapter, key_or_set)
+    local extend_set = key_or_set
+    if type(key_or_set) == 'string' then extend_set = { env = { api_key = api_key_cmd:format(key_or_set) } } end
+    return function() return require('codecompanion.adapters').extend(adapter, extend_set) end
+  end
+
+  require('codecompanion').setup({
+    adapters = {
+      http = {
+        ollama = extend_adapter('ollama', ollama_setting),
+        anthropic = extend_adapter('anthropic', 'ANTHROPIC_API_KEY'),
+        deepseek = extend_adapter('deepseek', 'DEEPSEEK_API_KEY'),
+        gemini = extend_adapter('gemini', 'GEMINI_API_KEY'),
+        openrouter = extend_adapter('openai_compatible', {
+          env = {
+            url = 'https://openrouter.ai/api',
+            api_key = api_key_cmd:format('OPENROUTER_API_KEY'),
+            chat_url = '/v1/chat/completions',
+          },
+          schema = { model = { default = openrouter_model } },
+        }),
+      },
+    },
+    interactions = {
+      background = { adapter = default_adapter },
+      chat = { adapter = default_adapter },
+      inline = { adapter = default_adapter },
+      cmd = { adapter = default_adapter },
+    },
+    extensions = { history = { enabled = true } },
+  })
   map({ 'n', 'v' }, '<leader>Ca', '<cmd>CodeCompanionActions<cr>', 'Open Code Companion actions menu')
   map({ 'n', 'v' }, '<leader>CC', '<cmd>CodeCompanionChat Toggle<cr>', 'Toggle Code Companion chat window')
   map('v', '<leader>CA', '<cmd>CodeCompanionChat Add<cr>', 'Add selection to Code Companion chat context')
