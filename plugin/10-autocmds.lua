34ea32017fde5611e46b0a5a0901a98ab307bb88
wenjin <hewenjin94@outlook.com>
2026-04-28 11:49:19 +0800

    fix(neovim): empty signcolumn in codecompanion cli buffer

--- plugin/10-autocmds.lua
+++ plugin/10-autocmds.lua
@@ -19,6 +19,7 @@ vim.schedule(function()
       vim.b.miniindentscope_disable = true
       vim.opt_local.number = false
       vim.opt_local.relativenumber = false
+    vim.opt_local.signcolumn = 'no'
     end,
   })
 
