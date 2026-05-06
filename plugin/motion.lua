d243bee73ecc0e9a749c4954a4498cb9e2ef7748
wenjin <hewenjin94@outlook.com>
2026-05-06 20:32:10 +0800

    chore(neovim): add mini.jump2d keymaps

--- plugin/motion.lua
+++ plugin/motion.lua
@@ -9,4 +9,8 @@ later(function()
   map({ 'n', 'x', 'o' }, 'ss', jump2d_single_chatacter, 'Jump2d single chatacter')
   local function jump2d_query() MiniJump2d.start(MiniJump2d.builtin_opts.query) end
   map({ 'n', 'x', 'o' }, 'sq', jump2d_query, 'Jump2d query')
+  local function jump2d_line_start() MiniJump2d.start(MiniJump2d.builtin_opts.line_start) end
+  map({ 'n', 'x', 'o' }, 'sl', jump2d_line_start, 'Jump2d line start')
+  local function jump2d_word_start() MiniJump2d.start(MiniJump2d.builtin_opts.word_start) end
+  map({ 'n', 'x', 'o' }, 'sw', jump2d_word_start, 'Jump2d word start')
 end)
