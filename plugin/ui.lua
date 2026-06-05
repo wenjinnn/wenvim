7d184ac6a401606c4c415e389d8744f3b820d5df
wenjin <hewenjin94@outlook.com>
2026-06-05 15:27:52 +0800

    feat(neovim): add mini.input settings

--- plugin/ui.lua
+++ plugin/ui.lua
@@ -28,6 +28,7 @@ starter.setup({
 })
 
 later(function()
+  require('mini.input').setup()
   require('mini.indentscope').setup({
     draw = {
       animation = require('mini.indentscope').gen_animation.none(),
